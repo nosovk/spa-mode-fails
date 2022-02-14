242ea161026adb9f4dfa9feb8e69dddc1c40c8f5
Konstantin Nosov <nosov@nodeart.io>
2022-02-14 15:17:33 +0200

    add config adapter with hydrate: false option

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		browser: {
+			hydrate: false,
+			router: false
 		}
 	}
 };
